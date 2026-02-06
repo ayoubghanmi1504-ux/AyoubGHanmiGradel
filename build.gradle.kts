8f644e463c6dc0a71dff0e3650ab92deb1783448
ayoub ghanmi <ayoubghanmi1504@gmail.com>
2026-02-06 20:15:51 +0100

    creacion del proyecto y las dependencias

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
+    implementation("dev.langchain4j:langchain4j-open-ai")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
